54533423cc021756ce7fb761797758bb7ef3a6dd
Sebastian Quek <sebq13@gmail.com>
2020-07-30 23:20:35 +0800

    add another var

--- printEnv.js
+++ printEnv.js
@@ -1,3 +1,4 @@
 console.log("Not so secret var", process.env.NOT_SO_SECRET_VAR);
 console.log("Secret var", process.env.SECRET_VAR);
 console.log("Secret var, uncensored?", JSON.stringify(process.env.SECRET_VAR));
+console.log("Another GitHub var", process.env.ANOTHER_VAR);
