94ce17484b94265078b9cebe015901fb564800ac
Sebastian Quek <sebq13@gmail.com>
2021-08-17 23:30:01 +0800

    update handler

--- handler.js
+++ handler.js
@@ -1,11 +1,16 @@
 'use strict';
 
 module.exports.hello = async (event) => {
+  const vars = {
+    offline_var_1: process.env.OFFLINE_VAR_1,
+    github_secrets_var_1: process.env.SECRET_VAR,
+    github_secrets_var_2: process.env.NOT_SO_SECRET_VAR,
+  }
   return {
     statusCode: 200,
     body: JSON.stringify(
       {
-        message: `var: ${process.env.OFFLINE_VAR_1}`,
+        message: `vars: ${JSON.stringify(vars)}`,
         input: event,
       },
       null,
